59d05f782a853a4f4585e833174f6a5139d3bda4
pimpf <anguel.off@gmail.com>
2015-08-07 10:57:03 +0300

    Second app

--- server.R
+++ server.R
@@ -1,5 +1,11 @@
 library(shiny)
 
-shinyServer(function(input, output){
+diabetesRisk <- function(glucose)
+    glucose / 200;
+
+shinyServer(
+    function(input, output) {
+        output$inputValue <- renderPrint({input$glucose})
+        output$prediction<- renderPrint(diabetesRisk({input$glucose}))
     }
 )
--- ui.R
+++ ui.R
@@ -2,8 +2,18 @@ library(shiny)
 
 shinyUI(
     pageWithSidebar(
-        headerPanel("Data Science FTW!"),
-        sidebarPanel(h3("Sidebar panel")),
-        mainPanel(h3("Main panel text"))
+        headerPanel("Illustrating inputs"),
+        sidebarPanel(
+            numericInput('glucose', 'Glucose mg/dl', 90, 
+                         min = 50, max = 200, step = 5),
+            submitButton("Submit")
+        ),
+        mainPanel(
+            h3('Results of prediction'),
+            h4('You entered'), 
+            verbatimTextOutput("inputValue"),
+            h4('Which resulted in a prediction of '), 
+            verbatimTextOutput("prediction")
+        )
     )
 )
