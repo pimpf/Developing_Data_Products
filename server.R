432d7ed1b0e378cc57b03a3c717e8c9649cb29c7
pimpf <anguel.off@gmail.com>
2015-08-21 20:08:21 +0300

    Final commit

--- server.R
+++ server.R
@@ -1,11 +1,87 @@
-library(shiny)
+###################################################################
+## File: server.R
+## Title: Course Project: Shiny Application and Reproducible Pitch
+## Author: Milen Angelov
+## Date: 20th August, 2015
+###################################################################
 
-diabetesRisk <- function(glucose)
-    glucose / 200;
+library(caret)
+library(ggplot2);
+library(MASS);
+library(kknn);
+library(gbm);
+library(plyr);
+library(mda);
+library(rpart);
+library(caTools);
 
-shinyServer(
-    function(input, output) {
-        output$inputValue <- renderPrint({input$glucose})
-        output$prediction<- renderPrint(diabetesRisk({input$glucose}))
-    }
-)
+data(iris);
+
+shinyServer(function(input, output) {
+    
+    v <- reactiveValues(drawResults = FALSE);
+    
+    observeEvent(input$draw, {
+        v$drawResults <- input$draw;
+    })
+    
+    output$predictions <- renderPrint({
+        if (v$drawResults == FALSE) 
+            return();
+        
+        isolate({
+            
+            withProgress(message = 'Progress...', value = 0, {
+                
+                ## Split data 
+                trainIndex <- createDataPartition(iris$Species, p=(input$data_split[1]/100), 
+                                                  list=FALSE);
+                
+                data_train <- iris[ trainIndex,];
+                data_test <- iris[-trainIndex,];
+                
+                incProgress(0.25, detail = "Defining train control");
+                
+                ## Define train control from the selection
+                tc <- if(input$tc == "Bootstrap"){
+                    trainControl(method="boot", number=100);
+                } else if (input$tc == "k-fold Cross Validation" ){
+                    trainControl(method="cv", number=10);        
+                } else if (input$tc == "Repeated k-fold Cross Validation" ){
+                    trainControl(method="repeatedcv", number=10, repeats=3);
+                } else {
+                    trainControl(method="LOOCV");
+                }
+                
+                incProgress(0.5, detail = "Computing model from the chosen method");
+                
+                ## Not sure if this is needed here, but let's assure reproducability
+                set.seed(54321);
+                
+                ## Fit model from the chosen method
+                model <- if(input$method == 1){
+                    train(Species~., data=data_train, trControl=tc, method="lda");
+                } else if(input$method == 2){
+                    train(Species~., data=data_train, trControl=tc, method="kknn");
+                } else if(input$method == 3){
+                    train(Species~., data=data_train, trControl=tc, method="gbm");
+                } else if(input$method == 4){
+                    train(Species~., data=data_train, trControl=tc, method="mda");
+                } else if (input$method == 5){
+                    train(Species~., data=data_train, trControl=tc, method="rpart");
+                } else {
+                    train(Species~., data=data_train, trControl=tc, method="LogitBoost");
+                }
+                
+                incProgress(0.75, detail = "Making predictions on test data");
+                
+                ## Make predictions
+                predictions <- predict(model, newdata = data_test);
+                
+                ## Print Confusion matrix
+                confusionMatrix(predictions, data_test$Species);
+
+            })
+        })
+    })
+})
--- ui.R
+++ ui.R
@@ -1,19 +1,46 @@
-library(shiny)
+###################################################################
+## File: ui.R
+## Title: Course Project: Shiny Application and Reproducible Pitch
+## Author: Milen Angelov
+## Date: 20th August, 2015
+###################################################################
+
+library(shiny);
+library(e1071);
 
 shinyUI(
-    pageWithSidebar(
-        headerPanel("Illustrating inputs"),
-        sidebarPanel(
-            numericInput('glucose', 'Glucose mg/dl', 90, 
-                         min = 50, max = 200, step = 5),
-            submitButton("Submit")
-        ),
-        mainPanel(
-            h3('Results of prediction'),
-            h4('You entered'), 
-            verbatimTextOutput("inputValue"),
-            h4('Which resulted in a prediction of '), 
-            verbatimTextOutput("prediction")
+    fluidPage(
+        titlePanel("Basic Caret package demo"),
+        
+        sidebarLayout(
+            sidebarPanel(
+                sliderInput("data_split", label = "train <-> test split", min = 50, 
+                            max = 100, value = c(75, 25)),
+                hr(),
+                selectInput("tc", "Train Control:", 
+                            choices = c("Bootstrap", 
+                                        "k-fold Cross Validation",
+                                        "Repeated k-fold Cross Validation",
+                                        "Leave One Out Cross Validation"),
+                            selected = "Bootstrap"),
+                hr(),
+                radioButtons("method", 
+                             label = "Train method",
+                             choices = list("Linear Discriminant Analysis" = 1, 
+                                            "k-Nearest Neighbors" = 2, 
+                                            "Stochastic Gradient Boosting" = 3,
+                                            "Mixture Discriminant Analysis" = 4,
+                                            "CART" = 5,
+                                            "Boosted Logistic Regression" = 6
+                             ), 
+                             selected = 1),
+                hr(),
+                actionButton("draw", "Show results")
+            ),
+            mainPanel(
+                h3("Confusion matrix and associated statistics for the model fit:"),
+                verbatimTextOutput('predictions')
+            )
         )
     )
 )
